094343736d3baa764d7703855a5008a3d96b5efd
Gary He <670684289@qq.com>
2026-02-04 17:01:27 +0800

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,26 +1,27 @@
+
 import React from 'react';
 import { HashRouter, Routes, Route } from 'react-router-dom';
-import { Layout } from './Layout';        // 修改：去掉了 ./components/
-import { Home } from './Home';            // 修改：去掉了 ./pages/
-import { BlogList } from './BlogList';    // 修改：去掉了 ./pages/
-import { PostDetail } from './PostDetail';// 修改：去掉了 ./pages/
-import { Admin } from './Admin';          // 修改：去掉了 ./pages/
-import { Feed } from './Feed';            // 修改：去掉了 ./pages/
+import { Layout } from './components/Layout';
+import { Home } from './pages/Home';
+import { BlogList } from './pages/BlogList';
+import { PostDetail } from './pages/PostDetail';
+import { Admin } from './pages/Admin';
+import { Feed } from './pages/Feed';
 
 const App: React.FC = () => {
-  return (
-    <HashRouter>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/blog" element={<BlogList />} />
-          <Route path="/post/:id" element={<PostDetail />} />
-          <Route path="/admin" element={<Admin />} />
-          <Route path="/feed" element={<Feed />} />
-        </Routes>
-      </Layout>
-    </HashRouter>
-  );
+  return (
+    <HashRouter>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/blog" element={<BlogList />} />
+          <Route path="/post/:id" element={<PostDetail />} />
+          <Route path="/admin" element={<Admin />} />
+          <Route path="/feed" element={<Feed />} />
+        </Routes>
+      </Layout>
+    </HashRouter>
+  );
 };
 
 export default App;
